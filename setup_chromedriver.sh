92d0e54930018cf13d421418a76f7114ed68a43f
Paul Tinsley <paul.tinsley@gmail.com>
2018-05-22 09:52:50 -0500

    need chrome for chromedriver

--- setup_chromedriver.sh
+++ setup_chromedriver.sh
@@ -1,6 +1,29 @@
 set -e
-apt update && apt install -y unzip libnss3
 cd /tmp
+
+#get prereqs in place
+apt update
+apt install -y \
+  unzip \
+  libnss3 \
+  xvfb \
+  libxss1 \
+  libappindicator1 \
+  libindicator7 \
+  fonts-liberation \
+  libappindicator3-1 \
+  libasound2 \
+  libatk-bridge2.0-0 \
+  libgtk-3-0 \
+  libxtst6 \
+  lsb-release \
+  xdg-utils
+
+#install chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome*.deb
+
+#install chromedriver
 wget -NL https://chromedriver.storage.googleapis.com/2.38/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 chmod +x chromedriver
